9f68581854041a8974194a4a49ba8acbef875369
AmirHossein <amir721asd@gmail.com>
2023-04-04 15:36:46 +0330

    golang

--- main.go
+++ main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	// Log items to the terminal
+	"log"
+	// Import gin for route definition
 	"github.com/gin-gonic/gin"
-	"net/http"
+	// Import godotenv for .env variables
+	"github.com/joho/godotenv"
+	// Import our app controllers
+	"gin-mongo-api/app/Http/controllers"
+	// Import our app routes
+	"gin-mongo-api/routes"
 )
 
+// init gets called before the main function
+func init() {
+	// Log error if .env file does not exist
+	if err := godotenv.Load(); err != nil {
+		log.Printf("No .env file found")
+	}
+}
+
 func main() {
-	r := gin.Default()
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Hello Devak",
-		})
+	// Init gin router
+	router := gin.Default()
+	routes.UserRoute(router)
+	// Its great to version your API's
+	v1 := router.Group("/api/v1")
+	{
+		// Define the hello controller
+		hello := new(controllers.HelloWorldController)
+		// Define a GET request to call the Default
+		// method in controllers/hello.go
+		v1.GET("/hello", hello.Default)
+	}
+
+	// Handle error response when a route is not defined
+	router.NoRoute(func(c *gin.Context) {
+		// In gin this is how you return a JSON response
+		c.JSON(404, gin.H{"message": "Not found"})
 	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	// Init our server
+	router.Run(":5000")
 }
